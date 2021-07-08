2f221cd034e0a8aaa8f39fc96f30c2a68d638796
Kriska939 <cristinacorrado93@gmail.com>
2021-07-08 15:17:11 +0200

    added recap comment

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* RIEPILOGO logica esercizio:
+
+1 - Chiedere all'utente:
+    - nome e cognome (stringa)
+    - numero dei chilometri da percorrere (numero)
+    - fascia d'età del passeggero (tendina - opzioni obbligatorie)
+
+2 - Utilizzare il dato "fascia d'età" per:
+    - calcolare prezzo del biglietto standard (0.21 *n km)
+    - calcolare prezzo del biglietto minorenne (-20%)
+    - calcolare prezzo del biglietto over65 (-40%)
+
+3 - Stampare dettagli biglietto DOPO evento click:
+    -nome passeggero
+    -tipo di offerta
+    -n carrozza (random 1-12)
+    -codice treno (n arbitrario fisso)
+    -prezzo finale (2 decimali)
+
+Bonus:
+- nascondere la sezione del biglietto se non è ancora stato generato il biglietto stesso
+- aggiungere una funzione che ci permetta di resettare i campi del form ai valori originali
+
+*/
